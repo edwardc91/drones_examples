93430a237667952c2d88f000dc45dee9426d0df5
Eduardo Miguel Hernandez <emiguelc1991@protonmail.com>
2022-05-24 12:12:05 -0400

    Allowed all hosts on django settings

--- api/drones/settings.py
+++ api/drones/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-prrcpkjrky2p7%$9@%=g@@(h7f&*xfo$bu-m*_zk2f2q5jhl!7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
